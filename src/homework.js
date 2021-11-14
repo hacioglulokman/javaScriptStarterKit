827bfaa36453047b874c794011813ad894b7b175
hacioglulokman <73646720+hacioglulokman@users.noreply.github.com>
2021-11-14 19:48:48 +0300

    third

--- src/homework.js
+++ src/homework.js
@@ -23,4 +23,62 @@ function findPrime(...numbers) {
 
     }
 }
-findPrime(2,5,8,21,13)
+findPrime(2,17,129,547,58)
+
+
+//arkadas sayılar
+
+function friendNumberFind(sayi1,sayi2) {
+
+    let totalSayi1=0
+    let totalSayi2=0
+
+    for (let i= 0; i < sayi1; i++) {
+
+        let tamBölen = 0
+    
+        if (sayi1 % i == 0) 
+        {
+            totalSayi1+=i
+            
+        } 
+        
+    }
+
+    for (let j= 0; j < sayi2; j++) {
+
+        let tamBölen = 0
+    
+        if (sayi2 % j == 0) 
+        {
+            totalSayi2+=j
+            
+        } 
+        
+    }
+    
+    if (totalSayi1==sayi1 & totalSayi2==sayi2){
+        console.log(sayi1 + " ve " + sayi2 + " arkadaş sayılardır.")
+    }else{
+        console.log(sayi1 + " ve " + sayi2 + " arkadaş sayılar değildir.")
+    }
+    }
+
+
+friendNumberFind(220,284)
+
+// mükemmel sayılar 
+    for (let k = 1; k < 10; k++) {  
+        for (let m = 1; m < k; m++) {
+            let totalbölen = 0 
+            if (k % m ==0){
+                totalbölen+=m 
+            }
+            if (k*2 == totalbölen){
+                console.log(k + " mükemmel sayıdır.")
+
+            } else{
+                console.log(k + " mükemmel sayı değildir.")
+            }
+        }     
+    }
