3511fd492968b4a8b8088480bdcf27310626628e
hacioglulokman <73646720+hacioglulokman@users.noreply.github.com>
2021-11-14 16:09:50 +0300

    first

--- homework.js
+++ homework.js
@@ -1,4 +1,22 @@
-function findNumber(...numbers) {
-    
-    
+function findNPrime(...numbers) {
+
+    for (let i = 0; i< numbers.length; i++){    
+         let isPrime = 0
+         
+       for (let j = 2; j < numbers[i]; j++) {
+        if (numbers[i]%j==0)
+        isPrime++         
+           
+       }  
+    }
+
+    if (isPrime==0) {
+
+        console.log(numbers[i]+ "asaldır")
+        
+    }else{
+        console.log(numbers[i]+ "asal değildir.")
+    }
 }
+
+findNPrime(7,17)
